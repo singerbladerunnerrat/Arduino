6f2744a1b31e74c43ce46b0daf6bab8f7a648ea5
singerbladerunnerrat <81724699+singerbladerunnerrat@users.noreply.github.com>
2021-08-18 14:35:02 -0500

    Create 2.ino

---
+++ My Scripts/2.ino
@@ -0,0 +1,24 @@
+#include "Mouse.h"
+
+void setup()
+{
+  pinMode(2, INPUT_PULLUP);
+}
+
+void loop()
+{
+  // do nothing until pin 2 goes low
+  while (digitalRead(2) == HIGH)
+  {
+  }
+  
+  Mouse.begin();
+
+  while (digitalRead(2) == LOW)
+  {
+    Mouse.click();
+    delay(25);
+  }
+  
+  Mouse.end();
+}
